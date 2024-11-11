ba1069ff1a803961c2376a3ac761fe53ff116977
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2024-11-11 17:16:11 -0300

    feat: add wasm runtime config

--- beamstagram/config/config.exs
+++ beamstagram/config/config.exs
@@ -36,7 +36,7 @@ config :esbuild,
   version: "0.17.11",
   beamstagram: [
     args:
-      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+      ~w(js/app.js --bundle --target=es2022 --format=esm --loader:.wasm=file --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
   ]
